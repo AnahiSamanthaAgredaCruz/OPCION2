4f6c888cd4f8a4cd24fcbb4dac5c2cfe4763078d
Anahi Samantha Agreda Cruz <a2019118777@estudiantes.upsa.edu.bo>
2020-12-12 12:25:38 -0400

    Se creo el controlador

---
+++ Operacion/Operacion/Controllers/OperacionesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Operacion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperacionesController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Operaiones( int numero)
+        {
+            string respuesta = "";
+            i
+
+        }
+    }
+}
